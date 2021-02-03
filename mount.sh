c686ca33e739918a84bc4eac1d543d93e85d2eba
Fernando Bello M <fbello04@hotmail.com>
2021-02-03 12:50:09 -0600

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,6 +1,4 @@
  echo "Creando carpeta share"
  mkdir share
  echo "montando unidad"
- mount -t 9p -o trans=virtio,version=9p2000.L /share share
-cd share/ArchSes
-./install.sh
+ mount -t 9p -o trans=virtio,version=9p2000.L /share share 
