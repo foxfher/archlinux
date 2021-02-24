27ee7c76e43cd14383e4335af73d45047fdff691
Fernando Bello M <fbello04@hotmail.com>
2021-02-24 12:26:29 -0600

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,5 +1,9 @@
- echo "Aumentando Espacio cowspace"
- mount -o remount,size=4G /run/archiso/cowspace
+if [ $(df | grep -i "cowspace" ]; then
+ if [ $(df | grep -i "cowspace" | awk '{print $2}')!="4.0G" ]; then
+  echo "Aumentando Espacio cowspace"
+  mount -o remount,size=4G /run/archiso/cowspace
+ fi
+fi
  echo "Creando carpeta share"
  mkdir share
  echo "montando unidad"
