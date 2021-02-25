2f1c5928d312f32e41f06fc3fc01eb7bf85fa0ce
Fernando Bello M <fbello04@hotmail.com>
2021-02-25 00:42:06 -0600

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -6,5 +6,7 @@
     fi
  echo "Creando carpeta share"
  mkdir share
+ echo "Instalando opción de portapales en maquina virtual"
+ pacman -Sy spice-vdagent --noconfirm
  echo "montando unidad"
  mount -t 9p -o trans=virtio,version=9p2000.L /share share 
